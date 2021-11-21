b70536e2d145140706853b7981d081c143f5e6e5
janemzwangnj <janemzwangnj@gmail.com>
2021-11-21 18:17:24 -0500

    all button connected in a structure of mesh

--- connect-four.js
+++ connect-four.js
@@ -12,70 +12,161 @@ class Cell {
     this.id = id;
   }
 }
+//create a cells array to hold all buttons in a mesh
 const cells = [];
 const buttons = document.querySelectorAll('.btn');
 const columns = document.querySelectorAll('.column');
-for (let x = 0; x < columns.length; x++) {
-  for (let y = 0; y < columns[x].children.length; y++) {
+const numOfColumns = columns.length;
+console.log(numOfColumns);
+const numOfRows = columns[0].children.length;
+console.log(numOfRows);
+
+// define 9 functions for 9 cases
+// setNeighbor1 is the case of x=0 and y=0
+const setNeighbors1 = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = -1;
+  aCell.left = -1;
+  aCell.right = parseInt(aCell.id) + numOfRows;
+  aCell.topLeft = -1;
+  aCell.topRight = parseInt(aCell.id) + 1 + numOfRows;
+  aCell.bottomLeft = -1;
+  aCell.bottomRight = -1;
+};
+// setNeighbor2 is the case of x = 0 and y is the number of rows
+const setNeighbors2 = (aCell) => {
+  aCell.top = -1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = -1;
+  aCell.right = parseInt(aCell.id) + numOfRows;
+  aCell.topLeft = -1;
+  aCell.topRight = -1;
+  aCell.bottomLeft = -1;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + numOfRows;
+};
+//setNeighbors3 is the case of x = 0 and y is btw
+const setNeighbors3 = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = -1;
+  aCell.right = parseInt(aCell.id) + numOfRows;
+  aCell.topLeft = -1;
+  aCell.topRight = parseInt(aCell.id) + 1 + numOfRows;
+  aCell.bottomLeft = -1;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + numOfRows;
+};
+//setNeighbors4 is the case of x= number of columns, and y =0
+const setNeighbors4 = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = -1;
+  aCell.left = parseInt(aCell.id) - numOfRows;
+  aCell.right = -1;
+  aCell.topLeft = parseInt(aCell.id) + 1 - numOfRows;
+  aCell.topRight = -1;
+  aCell.bottomLeft = -1;
+  aCell.bottomRight = -1;
+};
+//setNeighbors5 is the case of x= number of columns, and y = number of rows
+const setNeighbors5 = (aCell) => {
+  aCell.top = -1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - numOfRows;
+  aCell.right = -1;
+  aCell.topLeft = -1;
+  aCell.topRight = -1;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - numOfRows;
+  aCell.bottomRight = -1;
+};
+//setNeighbors6 is the case of x= number of columns, and y is btw
+const setNeighbors6 = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - numOfRows;
+  aCell.right = -1;
+  aCell.topLeft = parseInt(aCell.id) + 1 - numOfRows;
+  aCell.topRight = -1;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - numOfRows;
+  aCell.bottomRight = -1;
+};
+//setNeighbors7 is the case of x is btw, and y = 0
+const setNeighbors7 = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = -1;
+  aCell.left = parseInt(aCell.id) - numOfRows;
+  aCell.right = parseInt(aCell.id) + numOfRows;
+  aCell.topLeft = parseInt(aCell.id) + 1 - numOfRows;
+  aCell.topRight = parseInt(aCell.id) + 1 + numOfRows;
+  aCell.bottomLeft = -1;
+  aCell.bottomRight = -1;
+};
+//setNeighbors8 is the case of x is btw, and y = number of rows
+const setNeighbors8 = (aCell) => {
+  aCell.top = -1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - numOfRows;
+  aCell.right = parseInt(aCell.id) + numOfRows;
+  aCell.topLeft = -1;
+  aCell.topRight = -1;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - numOfRows;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + numOfRows;
+};
+//setNeighbors9 is the case of x is btw, and y is btw
+const setNeighbors9 = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - numOfRows;
+  aCell.right = parseInt(aCell.id) + numOfRows;
+  aCell.topLeft = parseInt(aCell.id) + 1 - numOfRows;
+  aCell.topRight = parseInt(aCell.id) + 1 + numOfRows;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - numOfRows;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + numOfRows;
+};
+
+for (let x = 0; x < numOfColumns; x++) {
+  for (let y = 0; y < numOfRows; y++) {
     let aID = columns[x].children[y].getAttribute('id');
     let aCell = new Cell(aID);
     switch (x) {
       case 0:
-        //callFuncationA();
         switch (y) {
           case 0:
-            console.log('x = 0 and y=0');
+            setNeighbors1(aCell);
             break;
-          case columns[x].children.length - 1:
-            console.log('x=0 and y=max');
+          case numOfRows - 1:
+            setNeighbors2(aCell);
             break;
           default:
-            console.log('x=0 and y is btw');
+            setNeighbors3(aCell);
         }
-        //console.log(`here x is ${x}`);
         break;
-      case columns.length - 1:
-        //callFuncationB();
+      case numOfColumns - 1:
         switch (y) {
           case 0:
-            console.log('x=max and y = 0');
+            setNeighbors4(aCell);
             break;
-          case columns[x].children.length - 1:
-            console.log('x=max and y=max');
+          case numOfRows - 1:
+            setNeighbors5(aCell);
             break;
           default:
-            console.log('x=max and y is btw');
+            setNeighbors6(aCell);
         }
-        //console.log(`here x is ${x}`);
         break;
       default:
-        //callFuncationC();
         switch (y) {
           case 0:
-            console.log('x is btw and y = 0');
+            setNeighbors7(aCell);
             break;
-          case columns[x].children.length - 1:
-            console.log('x is btw and y = max');
+          case numOfRows - 1:
+            setNeighbors8(aCell);
             break;
           default:
-            console.log('x is btw and y is btw');
+            setNeighbors9(aCell);
         }
-      // console.log(`here x is ${x}`);
     }
+    cells.push(aCell);
   }
 }
-//console.log(aCell)
-
-const centerCells = (aCell) => {
-  aCell.top = parseInt(aCell.id) + 1;
-  aCell.bottom = parseInt(aCell.id) - 1;
-  aCell.left = parseInt(aCell.id) - columns[x].children.length;
-  aCell.right = parseInt(aCell.id) + columns[x].children.length;
-  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
-  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
-  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
-};
+console.log(cells);
 
 ////////////////////////////////
 // Functions For Game Logic Here
