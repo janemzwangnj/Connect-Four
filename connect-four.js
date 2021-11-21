db849bcff5bc26d80d34e792e22fb4ca52e510e2
janemzwangnj <janemzwangnj@gmail.com>
2021-11-21 16:18:27 -0500

    complete board mesh logic

--- connect-four.js
+++ connect-four.js
@@ -15,101 +15,57 @@ class Cell {
 const cells = [];
 const buttons = document.querySelectorAll('.btn');
 const columns = document.querySelectorAll('.column');
-//console.log(buttons);
-//console.log(columns);
 for (let x = 0; x < columns.length; x++) {
   for (let y = 0; y < columns[x].children.length; y++) {
     let aID = columns[x].children[y].getAttribute('id');
     let aCell = new Cell(aID);
-
-    if (x === 0) {
-      aCell.left = -1;
-      aCell.topLeft = -1;
-      aCell.bottomLeft = -1;
-      /*    if(y === 0){
-        aCell.bottom = -1;
-      } else aCell.bottom = parseInt(aCell.id) - 1;
-      if( y === columns[x].children.length - 1 ){
-
-      }
-
-      aCell.top = parseInt(aCell.id) + 1;
-      //aCell.bottom = parseInt(aCell.id) - 1;
-      aCell.left = -1;
-      aCell.right = parseInt(aCell.id) + columns[x].children.length;
-      aCell.topLeft = -1;
-      aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-      aCell.bottomLeft = -1;
-      aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length; */
-    } else if (x === columns.length - 1) {
-      aCell.right = -1;
-      aCell.topRight = -1;
-      aCell.bottomRight = -1;
-    } else if (y === 0) {
-      aCell.bottom = -1;
-      aCell.bottomLeft = -1;
-      aCell.bottomRight = -1;
-    } else if (y === columns[x].children.length - 1) {
-      aCell.topCells = -1;
-      aCell.topRight = -1;
-      aCell.topLeft = -1;
-    } else {
-      aCell.top = parseInt(aCell.id) + 1;
-      aCell.bottom = parseInt(aCell.id) - 1;
-      aCell.left = parseInt(aCell.id) - columns[x].children.length;
-      aCell.right = parseInt(aCell.id) + columns[x].children.length;
-      aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
-      aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-      aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
-      aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+    switch (x) {
+      case 0:
+        //callFuncationA();
+        switch (y) {
+          case 0:
+            console.log('x = 0 and y=0');
+            break;
+          case columns[x].children.length - 1:
+            console.log('x=0 and y=max');
+            break;
+          default:
+            console.log('x=0 and y is btw');
+        }
+        //console.log(`here x is ${x}`);
+        break;
+      case columns.length - 1:
+        //callFuncationB();
+        switch (y) {
+          case 0:
+            console.log('x=max and y = 0');
+            break;
+          case columns[x].children.length - 1:
+            console.log('x=max and y=max');
+            break;
+          default:
+            console.log('x=max and y is btw');
+        }
+        //console.log(`here x is ${x}`);
+        break;
+      default:
+        //callFuncationC();
+        switch (y) {
+          case 0:
+            console.log('x is btw and y = 0');
+            break;
+          case columns[x].children.length - 1:
+            console.log('x is btw and y = max');
+            break;
+          default:
+            console.log('x is btw and y is btw');
+        }
+      // console.log(`here x is ${x}`);
     }
-    console.log(aCell);
   }
 }
 //console.log(aCell)
-const bottomCells = (aCell) => {
-  aCell.top = parseInt(aCell.id) + 1;
-  aCell.bottom = -1;
-  aCell.left = parseInt(aCell.id) - columns[x].children.length;
-  aCell.right = parseInt(aCell.id) + columns[x].children.length;
-  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
-  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
-  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
-};
-const topCells = (aCell) => {
-  aCell.top = parseInt(aCell.id) + 1;
-  aCell.bottom = parseInt(aCell.id) - 1;
-  aCell.left = parseInt(aCell.id) - columns[x].children.length;
-  aCell.right = parseInt(aCell.id) + columns[x].children.length;
-  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
-  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
-  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
-};
-const leftCells = (aCell) => {
-  aCell.left = -1;
-  aCell.topLeft = -1;
-  aCell.bottomLeft = -1;
-  aCell.top = parseInt(aCell.id) + 1;
-  aCell.bottom = parseInt(aCell.id) - 1;
-  aCell.left = parseInt(aCell.id) - columns[x].children.length;
-  aCell.right = parseInt(aCell.id) + columns[x].children.length;
-  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
-  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
-  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
-};
-const rightCells = (aCell) => {
-  aCell.top = parseInt(aCell.id) + 1;
-  aCell.bottom = parseInt(aCell.id) - 1;
-  aCell.left = parseInt(aCell.id) - columns[x].children.length;
-  aCell.right = parseInt(aCell.id) + columns[x].children.length;
-  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
-  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
-  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
-  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
-};
+
 const centerCells = (aCell) => {
   aCell.top = parseInt(aCell.id) + 1;
   aCell.bottom = parseInt(aCell.id) - 1;
