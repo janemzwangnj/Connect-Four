9547fed544189c41049294f592c632a790a15f32
janemzwangnj <janemzwangnj@gmail.com>
2021-11-21 15:34:50 -0500

    create a class Cell and created 42 objects of Cell and mesh together

--- connect-four.js
+++ connect-four.js
@@ -8,34 +8,139 @@ class Cell {
   topLeft = '';
   bottomRight = '';
   bottomLeft = '';
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  constructor(id) {
+    this.id = id;
   }
 }
-const columns = document.getElementsByClassName('column');
-console.log(columns.length);
-for (let i = 0; i < columns.length; i++) {
-  let numOfChildren = columns[i].children.length;
-  for (let j = numOfChildren - 1; j >= 0; j--) {
-    /*  console.log(`[${i} ${j}]`);
-    console.log(columns[i].children[j]); */
-    const cell = new Cell(i, j);
-    //cell.top =
+const cells = [];
+const buttons = document.querySelectorAll('.btn');
+const columns = document.querySelectorAll('.column');
+//console.log(buttons);
+//console.log(columns);
+for (let x = 0; x < columns.length; x++) {
+  for (let y = 0; y < columns[x].children.length; y++) {
+    let aID = columns[x].children[y].getAttribute('id');
+    let aCell = new Cell(aID);
+
+    if (x === 0) {
+      aCell.left = -1;
+      aCell.topLeft = -1;
+      aCell.bottomLeft = -1;
+      /*    if(y === 0){
+        aCell.bottom = -1;
+      } else aCell.bottom = parseInt(aCell.id) - 1;
+      if( y === columns[x].children.length - 1 ){
+
+      }
+
+      aCell.top = parseInt(aCell.id) + 1;
+      //aCell.bottom = parseInt(aCell.id) - 1;
+      aCell.left = -1;
+      aCell.right = parseInt(aCell.id) + columns[x].children.length;
+      aCell.topLeft = -1;
+      aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+      aCell.bottomLeft = -1;
+      aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length; */
+    } else if (x === columns.length - 1) {
+      aCell.right = -1;
+      aCell.topRight = -1;
+      aCell.bottomRight = -1;
+    } else if (y === 0) {
+      aCell.bottom = -1;
+      aCell.bottomLeft = -1;
+      aCell.bottomRight = -1;
+    } else if (y === columns[x].children.length - 1) {
+      aCell.topCells = -1;
+      aCell.topRight = -1;
+      aCell.topLeft = -1;
+    } else {
+      aCell.top = parseInt(aCell.id) + 1;
+      aCell.bottom = parseInt(aCell.id) - 1;
+      aCell.left = parseInt(aCell.id) - columns[x].children.length;
+      aCell.right = parseInt(aCell.id) + columns[x].children.length;
+      aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
+      aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+      aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
+      aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+    }
+    console.log(aCell);
   }
 }
-let element = columns[0].children[0];
-//console.log(element);
+//console.log(aCell)
+const bottomCells = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = -1;
+  aCell.left = parseInt(aCell.id) - columns[x].children.length;
+  aCell.right = parseInt(aCell.id) + columns[x].children.length;
+  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
+  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+};
+const topCells = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - columns[x].children.length;
+  aCell.right = parseInt(aCell.id) + columns[x].children.length;
+  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
+  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+};
+const leftCells = (aCell) => {
+  aCell.left = -1;
+  aCell.topLeft = -1;
+  aCell.bottomLeft = -1;
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - columns[x].children.length;
+  aCell.right = parseInt(aCell.id) + columns[x].children.length;
+  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
+  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+};
+const rightCells = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - columns[x].children.length;
+  aCell.right = parseInt(aCell.id) + columns[x].children.length;
+  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
+  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+};
+const centerCells = (aCell) => {
+  aCell.top = parseInt(aCell.id) + 1;
+  aCell.bottom = parseInt(aCell.id) - 1;
+  aCell.left = parseInt(aCell.id) - columns[x].children.length;
+  aCell.right = parseInt(aCell.id) + columns[x].children.length;
+  aCell.topLeft = parseInt(aCell.id) + 1 - columns[x].children.length;
+  aCell.topRight = parseInt(aCell.id) + 1 + columns[x].children.length;
+  aCell.bottomLeft = parseInt(aCell.id) - 1 - columns[x].children.length;
+  aCell.bottomRight = parseInt(aCell.id) - 1 + columns[x].children.length;
+};
 
 ////////////////////////////////
 // Functions For Game Logic Here
+const inputRedBlack = (aColumn, x) => {
+  //console.log(x);
+  const aButton = document.getElementById('1');
+  let arr = aButton.classList;
+  arr.add('black');
+  // console.log(arr);
+  //console.log(document.getElementsByClassName('black'));
+  //console.log(aButton.classList[1]);
+  //console.log(aButton);
+};
 
 ////////////////////////////////
 // Event Listeners Here
 const gameStart = () => {
   for (let i = 0; i < columns.length; i++) {
     columns[i].addEventListener('click', () => {
-      console.log('Show me something');
+      //  console.log(columns[i]);
+      inputRedBlack(columns[i], i);
     });
   }
 };
